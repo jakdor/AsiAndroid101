44a6611513415b75ae359cc41cc88bb7240fd2cf
Jakub Dorda <jak_dor@wp.pl>
2017-11-16 20:02:15 +0100

    spotkanie2 - fragment dziła

--- app/src/main/java/com/asi/kursasiandek/Fragment1.java
+++ app/src/main/java/com/asi/kursasiandek/Fragment1.java
@@ -1,15 +1,28 @@
 package com.asi.kursasiandek;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
+import butterknife.BindView;
 import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class Fragment1 extends Fragment {
+
+    @BindView(R.id.button1)
+    Button button;
+    @BindView(R.id.editBox)
+    EditText editText;
+    @BindView(R.id.out)
+    TextView textView;
 
     @Nullable
     @Override
@@ -20,5 +33,19 @@ public class Fragment1 extends Fragment {
         View view = inflater.inflate(R.layout.fragment1_layout,container,false);
         ButterKnife.bind(this,view);
         return view;
+    }
+
+    @OnClick(R.id.button1)
+    void onButtonClick(){
+        String str = editText.getText().toString();
+        if (!str.isEmpty()) {
+            textView.setText(str);
+        }
+    }
+
+    @OnClick(R.id.new_activity_button)
+    void onNewActivityButtonClick(){
+        Intent intent = new Intent(getContext(), Activity2.class);
+        startActivity(intent);
     }
 }
--- app/src/main/java/com/asi/kursasiandek/MainActivity.java
+++ app/src/main/java/com/asi/kursasiandek/MainActivity.java
@@ -1,43 +1,38 @@
 package com.asi.kursasiandek;
 
-import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.TextView;
 
-import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
-
-    @BindView(R.id.button1)
-    Button button;
-    @BindView(R.id.editBox)
-    EditText editText;
-    @BindView(R.id.out)
-    TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
+
+        getSupportFragmentManager()
+                .beginTransaction()
+                .add(R.id.fragment_container, new Fragment1())
+                .commit();
     }
 
-    @OnClick(R.id.button1)
-    void onButtonClick(){
-        String str = editText.getText().toString();
-        if (!str.isEmpty()) {
-            textView.setText(str);
-        }
+    @OnClick(R.id.frag1)
+    void onButton1Click(){
+        getSupportFragmentManager()
+                .beginTransaction()
+                .replace(R.id.fragment_container, new Fragment1())
+                .commit();
     }
 
-    @OnClick(R.id.new_activity_button)
-    void onNewActivityButtonClick(){
-        Intent intent = new Intent(this, Activity2.class);
-        startActivity(intent);
+    @OnClick(R.id.frag2)
+    void onButton2Click(){
+        getSupportFragmentManager()
+                .beginTransaction()
+                .replace(R.id.fragment_container, new Fragment2())
+                .commit();
     }
 }
